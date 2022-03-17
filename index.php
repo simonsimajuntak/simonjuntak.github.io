96b6c60a6a34482202e8218fa0e61610fe8d15a7
Simon Rioland S <101165384+simonjuntak@users.noreply.github.com>
2022-03-17 12:49:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,196 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" type="text/css" href="1.css">
+
+    <title>My Portfolio | Simon Rioland S</title>
+     <link rel="shortcut icon" type="image/png" href="img/wd.jpg">
+  </head>
+  <body id="home">
+    
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light shadow-lg fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">SIMON RIOLAND S</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home"><strong>Home</strong></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#about"><strong>About</strong></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#project"><strong>Project</strong></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#contact"><strong>Contact</strong></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <!-- Akhir Navbar -->
+
+ 
+    
+    <!-- Awal Jumbotron -->
+    <section class="jumbotron text-center">
+      <img src="img/wd.jpg" alt="Simon Rioland S" width="300" class="rounded-circle">
+      <h1 class="display-4"><strong>SIMON RIOLAND S</strong></h1>
+      <p class="lead"><strong>Fresh Gruduate | Bachelor of Physics 
+      <br/>(Instrumentation Electronics, Komputing and Nuclear)</strong></p>
+    </section>
+
+
+    <section id="about">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h2>PROFESSIONAL SUMMARY</h2>
+            <br/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <p align="justify">&emsp;&emsp;&emsp;&emsp;
+            Saya seorang fresh graduate lulusan Universitas Sriwijaya dengan Jurusan Fisika dengan bidang minat Elektronika Instrumentasi, Komputasi dan Nuklir dengan predikat sangat memuaskan. Saya aktif dalam kegiatan akademik dan organisasi yang ada di kampus, tergabung kedalam HIMAFIA (Himpunan Mahasiswa Fisika Universitas Sriwijaya) tahun 2017-2021 sebagai Ketua KOMDIS LDO 2018 dan PDO (Persekutuan Doa Oikumene) Immanuel tahun 2017-2021 sebagai Ketua Seksi Peralatan 2019-2020. Saya terpilih menjadi Asisten dan Koordinator Asisten Laboratorium Elektronika Universitas Sriwijaya tahun 2020-2021 dan memperoleh Beasiswa Talenta Inovasi Indonesia 2021. Dengan pengalaman serta pencapaian yang saya peroleh membentuk saya menjadi pribadi yang mandiri, ulet dan suka dengan tantangan, hal ini yang mendorong saya untuk terus belajar, berkomitmen dan bekerja keras.</p>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir About -->
+    <br/>
+    <br/>
+
+    <!-- Project -->
+    <section id="project">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h4>PROJECT</h4>
+            <br/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/6.PNG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">SISTEM KENDALI</h5>
+                <!-- <p class="card-text">HERBS AND SPICES</p> -->
+                <a href="#" class="btn btn-primary">See</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/7.PNG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Herbs And Spices</h5>
+                <!-- <p class="card-text">HERBS AND SPICES</p> -->
+                <a href="#" class="btn btn-primary">See</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/8.PNG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Herbs And Spices</h5>
+                <!-- <p class="card-text">HERBS AND SPICES</p> -->
+                <a href="#" class="btn btn-primary">See</a>
+              </div>
+            </div>
+          </div><br/>
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/9.PNG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Herbs And Spices</h5>
+                <!-- <p class="card-text">HERBS AND SPICES</p> -->
+                <a href="#" class="btn btn-primary">See</a>
+              </div>
+            </div>
+          </div><br/>
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/10.PNG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Herbs And Spices</h5>
+                <!-- <p class="card-text">HERBS AND SPICES</p> -->
+                <a href="#" class="btn btn-primary">See</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/11.PNG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Herbs And Spices</h5>
+                <!-- <p class="card-text">HERBS AND SPICES</p> -->
+                <a href="#" class="btn btn-primary">See</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir Project -->
+    <br/>
+    <br/>
+
+    <!-- Contact-->
+    <section id="contact">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h6>Contact Us</h6>
+          </div>
+        </div>
+
+        <!-- <div class="row text-center">
+          <div class="col">
+            <p>Call us today at (+62) 81 8051 89 888<br/>
+              Instagram @indonesianexportnusantara <br/>
+            Email us at aisyahcahayanusantara@gmail.com</p>
+
+            <p>Office<br/>
+            Jalan. Urip Sumoharjo No.46 48, Embong Kaliasin,
+            <br/>Kec. Genteng, Kota SBY, Jawa Timur 60271</p>
+
+            <p>Manufacture<br/>
+              Jalan Raya Sumbersuko No.08 Lumajang Regency,<br/>
+            East Java Indonesia 67316</p>
+          </div> -->
+          <div>
+           <a href="https://linkedin.com/in/simon-rioland-s-7a6177227" target="_blank"><buttonid="close" class="linkedin" onClick="javascript:close_clip()"><img src="img/15.png" width="50"></button></a>&emsp;&emsp;
+           <a href="https://linkedin.com/in/simon-rioland-s-7a6177227" target="_blank"><buttonid="close" class="linkedin" onClick="javascript:close_clip()"><img src="img/22.png" width="50"></button></a> 
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir Contact -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
