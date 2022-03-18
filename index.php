008084f446af1a7e3261d1e825d5fcb06de6c682
Simon Rioland S <101165384+simonjuntak@users.noreply.github.com>
2022-03-18 14:38:45 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,194 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" type="text/css" href="1.css">
+
+    <title>My Portfolio | Simon Rioland S</title>
+     <link rel="shortcut icon" type="image/png" href="img/wd.jpg">
+  </head>
+  <body id="home">
+    
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light shadow-lg fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">SIMON RIOLAND S</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home"><strong>Home</strong></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#about"><strong>About</strong></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#project"><strong>Project</strong></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#contact"><strong>Contact</strong></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <!-- Akhir Navbar -->
+
+ 
+    
+    <!-- Awal Jumbotron -->
+    <section class="jumbotron text-center">
+      <img src="img/wd.jpg" alt="Simon Rioland S" width="300" class="rounded-circle">
+      <h1 class="display-4"><strong>SIMON RIOLAND S</strong></h1>
+      <p class="lead"><strong>Fresh Gruduate | Bachelor of Physics 
+      <br/>(Instrumentation Electronics, Komputing and Nuclear)</strong></p> <br>
+      <div>
+        <a href="https://www.instagram.com/simonrioland/" target="_blank"><buttonid="close" class="linkedin" onClick="javascript:close_clip()"><img src="img/15.png" width="50"></button></a>&emsp;&emsp;
+        <a href="https://linkedin.com/in/simon-rioland-s-7a6177227" target="_blank"><buttonid="close" class="linkedin" onClick="javascript:close_clip()"><img src="img/22.png" width="50"></button></a>&emsp;&emsp;
+          <a href="https://drive.google.com/file/d/1SK_54k-vySlW0sP437SWqrdjKDXAhgK2/view?usp=sharing" target="_blank"><buttonid="close" class="linkedin-pdf" onClick="javascript:close_clip()"><img src="img/23.png" width="50"></button></a>
+      </div>
+    </section>
+
+
+    <section id="about">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h2>PROFESSIONAL SUMMARY</h2>
+            <br/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <p align="justify">&emsp;&emsp;&emsp;&emsp;
+            I am a fresh graduate from Sriwijaya University with a major in Physics with an interest in Instrumentation, Computing, and Nuclear Electronics with a very satisfactory predicate. I am active in academic and organizational activities on campus, joined HIMAFIA (Sriwijaya University Physics Student Association) in 2017-2021 as Chair of KOMDIS LDO 2018 and PDO (Oikumene Prayer Fellowship) Immanuel in 2017-2021 as Chair of the 2019-2020 Equipment Section. I was selected to be the Assistant and Coordinator of the Electronics Laboratory Assistant at Sriwijaya University in 2020-2021 and received the 2021 Indonesian Innovation Talent Scholarship. With the experience and achievements I have gained, I have shaped me into an independent, tenacious person who likes challenges, this is what encourages me to continuously learn, committing and work hard</p>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir About -->
+    <br/>
+    <br/>
+
+    <!-- Project -->
+    <section id="project">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h4>PROJECT</h4>
+            <br/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/24.jpg" class="card-img-top" alt="..." width="500">
+              <div class="card-body">
+                <h5 class="card-title">Sistem Kendali Berbasis IoT</h5>
+                <a href="https://drive.google.com/file/d/1k9xUVEi-tv_SLtNDgF3zIIvxNC_8clYp/view?usp=sharing" class="btn btn-primary" target="_blank"><buttonid="close" class="linkedin" onClick="javascript:close_clip()"></button>see</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/25.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Laboratorium Elektronika</h5>
+                <a href="https://drive.google.com/file/d/1G_N05GwXfx-7IUCN8oNK9yhDIdUzA5hV/view?usp=sharing" class="btn btn-primary" target="_blank"><buttonid="close" class="linkedin" onClick="javascript:close_clip()"></button>see</a>
+              </div>
+            </div>
+          </div>
+         <!--  <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/8.PNG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Herbs And Spices</h5>
+                <a href="#" class="btn btn-primary">See</a>
+              </div>
+            </div>
+          </div><br/>
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/9.PNG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Herbs And Spices</h5>
+                <a href="#" class="btn btn-primary">See</a>
+              </div>
+            </div>
+          </div><br/>
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/10.PNG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Herbs And Spices</h5>
+                <a href="#" class="btn btn-primary">See</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card";>
+              <img src="img/11.PNG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Herbs And Spices</h5>
+                <a href="#" class="btn btn-primary">See</a>
+              </div>
+            </div>
+          </div> -->
+        </div>
+      </div>
+    </section>
+    <!-- Akhir Project -->
+    <br/>
+    <br/>
+
+    <!-- Contact-->
+    <section id="contact">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h6>Contact:</h6>
+            <h6>simonrioland77@gmail.com</h6>
+            <h6>Jl. Anggrek Cendrawasi Raya G No. 8A, Kemanggisan, Palmerah, Kota Jakarta Barat.</h6>
+          </div>
+        </div>
+
+        <!-- <div class="row text-center">
+          <div class="col">
+            <p>Call us today at (+62) 81 8051 89 888<br/>
+              Instagram @indonesianexportnusantara <br/>
+            Email us at aisyahcahayanusantara@gmail.com</p>
+
+            <p>Office<br/>
+            Jalan. Urip Sumoharjo No.46 48, Embong Kaliasin,
+            <br/>Kec. Genteng, Kota SBY, Jawa Timur 60271</p>
+
+            <p>Manufacture<br/>
+              Jalan Raya Sumbersuko No.08 Lumajang Regency,<br/>
+            East Java Indonesia 67316</p>
+          </div> -->
+          
+        </div>
+      </div>
+    </section>
+    <!-- Akhir Contact -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
